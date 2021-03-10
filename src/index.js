7b1ab34c92f83b139f7846be8104710f08b3a25e
Ellis-sd <elvina.salimiva.00@bk.ru>
2021-03-10 19:01:50 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
 exports.min = function min (array) {
+  if (!array || !array.length) {
   return 0;
-}
-
-exports.max = function max (array) {
+  }
+  
+  return array.sort((a, b) => a - b)[0];
+  }
+  
+  exports.max = function max (array) {
+  if (!array || !array.length) {
   return 0;
-}
-
-exports.avg = function avg (array) {
+  }
+  
+  return array.sort((a, b) => a - b)[array.length - 1];
+  }
+  
+  exports.avg = function avg (array) {
+  if (!array || !array.length) {
   return 0;
-}
+  }
+  
+  return array.reduce((a, c) => a + c, 0)/array.length;
+  }
